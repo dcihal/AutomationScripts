fcf4b2feed1f716af9cc492ffc6b6924877aacba
Josh Lesch <jlesch@hayneedle.com>
2013-04-17 13:41:23 -0500

    Adding comment to the master method file

--- Hayneedle_Automation/features/step_definitions/master_method_tests.rb
+++ Hayneedle_Automation/features/step_definitions/master_method_tests.rb
@@ -1,10 +1,16 @@
+####This is our master test file where all basic web navigation is done.####
+####If you make changes to this file, make sure you are closes all of your def's correctly####
+####Not doing so will break all tests using this file.####
+
 module MasterMethodTests
 
+  #This method will go to the site/environment that you define below.
   def goto_hayneedle_site
-    #@browser = Watir::Browser.new :chrome
+    #the @browser variable is being defined the in the hooks.rb file
     @browser.goto('www.hayneedle.com')
   end
 
+  #Calling this method will close the popup that happens when you go to hayneedle with a clean session(first time visitor).
   def close_popup
     sleep 1
     #check to see if survey modal pops up
@@ -14,6 +20,7 @@ module MasterMethodTests
     sleep 1
   end
 
+  #This method will close a browser
   def close_browser
     @browser.close
   end
